4a42df2f464b621f22db3042b88c2d5a1663ec81
mvn-sonnguyen4-hn <son.nguyen4@monstar-lab.com>
2022-06-08 17:30:26 +0700

    first commit

--- index.js
+++ index.js
@@ -84,6 +84,12 @@ io.on("connection", (socket) => {
 });
 
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log("App is running", PORT);
